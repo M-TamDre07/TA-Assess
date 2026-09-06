8b23b329a5ff1fb7614d2c0512220a681d5f904c
M-TamDre07 <tamaandrea92@gmail.com>
2026-09-06 13:54:15 +0700

    Integrasikan Question Bank Web App ke runtime frontend

--- js/runtime-config.js
+++ js/runtime-config.js
@@ -9,11 +9,11 @@
 
     CONFIG.GOOGLE_SHEETS_API = 'https://script.google.com/macros/s/AKfycbw1_jurj5YOX_uO5Gyxk4X4FCVkhytFrsruTB5y3zpQHS4qy0euIgyjiPkkYLYt9eRf/exec';
     CONFIG.ACCOUNT_API = '';
-    CONFIG.QUESTION_BANK_API = '';
+    CONFIG.QUESTION_BANK_API = 'https://script.google.com/macros/s/AKfycbyT0jepU01BljPXNgMyUaAkgQ5U-j8X5n_kjh3pCosMhOv6hUAUA6uKETaAn7OlXTK9/exec';
     CONFIG.FORMSPREE_LINK = 'https://formspree.io/f/xaeydera';
     CONFIG.SAWERIA_LINK = 'https://saweria.co/tamaandrea';
 
     // ACCOUNT_API diisi setelah account.gs dideploy sebagai Web App.
-    // QUESTION_BANK_API diisi setelah question-bank.gs dideploy sebagai Web App.
+    // QUESTION_BANK_API adalah endpoint publik untuk membaca soal ACTIVE/PILOT/DEMO.
     // Jangan taruh secret atau admin key di file ini karena frontend bersifat publik.
 })();
