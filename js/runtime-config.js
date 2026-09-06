2dba0099137a9017dfa9946cd918859c5264c224
M-TamDre07 <tamaandrea92@gmail.com>
2026-09-06 13:57:46 +0700

    Gunakan Vercel proxy untuk Question Bank agar integrasi frontend lebih stabil

--- js/runtime-config.js
+++ js/runtime-config.js
@@ -9,11 +9,12 @@
 
     CONFIG.GOOGLE_SHEETS_API = 'https://script.google.com/macros/s/AKfycbw1_jurj5YOX_uO5Gyxk4X4FCVkhytFrsruTB5y3zpQHS4qy0euIgyjiPkkYLYt9eRf/exec';
     CONFIG.ACCOUNT_API = '';
-    CONFIG.QUESTION_BANK_API = 'https://script.google.com/macros/s/AKfycbyT0jepU01BljPXNgMyUaAkgQ5U-j8X5n_kjh3pCosMhOv6hUAUA6uKETaAn7OlXTK9/exec';
+    CONFIG.QUESTION_BANK_API = '/api/question-bank';
     CONFIG.FORMSPREE_LINK = 'https://formspree.io/f/xaeydera';
     CONFIG.SAWERIA_LINK = 'https://saweria.co/tamaandrea';
 
     // ACCOUNT_API diisi setelah account.gs dideploy sebagai Web App.
-    // QUESTION_BANK_API adalah endpoint publik untuk membaca soal ACTIVE/PILOT/DEMO.
+    // Question Bank dibaca melalui Vercel Function agar frontend tidak bergantung
+    // pada CORS ContentService Apps Script secara langsung.
     // Jangan taruh secret atau admin key di file ini karena frontend bersifat publik.
 })();
