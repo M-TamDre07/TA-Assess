45343be96f222ee611faee9ea040c72a1a0b6b58
M-TamDre07 <tamaandrea92@gmail.com>
2026-09-06 16:21:26 +0700

    fix: align assessment session audit with latest Apps Script backend

--- api/assessment-session.js
+++ api/assessment-session.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-const UPSTREAM = 'https://script.google.com/macros/s/AKfycbw1_jurj5YOX_uO5Gyxk4X4FCVkhytFrsruTB5y3zpQHS4qy0euIgyjiPkkYLYt9eRf/exec';
+const UPSTREAM = 'https://script.google.com/macros/s/AKfycbybvP-FJvO1ruHoGjikM60Y99ofiu9YrWkIXgl410ua1sxt96sgt8tCXCRYzLy8bwEx/exec';
 const RATE_WINDOW_MS = 60 * 1000;
 const MAX_ISSUES_PER_WINDOW = 12;
 const rateBuckets = new Map();
